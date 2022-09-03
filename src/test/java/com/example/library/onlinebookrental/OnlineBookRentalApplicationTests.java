00451a8ab5e9b67762b71084b25d2e57d87d71a7
abrah <abraham.berhe@gmail.com>
2022-09-03 17:53:12 -0400

    Spring boot starting code

---
+++ src/test/java/com/example/library/onlinebookrental/OnlineBookRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.library.onlinebookrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
