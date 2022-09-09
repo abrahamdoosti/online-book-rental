fe15127b39b219c60ab95bdba9e384df58506332
abrah <abraham.berhe@gmail.com>
2022-09-08 21:54:08 -0400

    ModelMapper, remote debug enable, UserResponseDTO added

--- src/main/java/com/example/onlinebookrental/app/OnlineBookRental.java
+++ src/main/java/com/example/onlinebookrental/app/OnlineBookRental.java
@@ -1,6 +1,5 @@
 package com.example.onlinebookrental.app;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -8,20 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.onlinebookrental.model.User;
-import com.example.onlinebookrental.service.UserService;
-
-import net.bytebuddy.dynamic.DynamicType.Builder.FieldDefinition.Optional;
-
 @RestController
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class OnlineBookRental {
-	
-	
+
 	@GetMapping("/index")
-	public ResponseEntity<String> helloWorld(){
-		return new ResponseEntity<>("Hello you have reached test page",HttpStatus.OK);
+	public ResponseEntity<String> helloWorld() {
+		return new ResponseEntity<>("Hello you have reached test page", HttpStatus.OK);
 	}
-	
-	
+
 }
